8e86fb324097d78c61be17d3176044c7f9c40c07
Tiago Peres França <tiago@nextel-thiago.local>
2019-05-10 15:19:17 -0300

    small corrections

--- src/actions/static.ts
+++ src/actions/static.ts
@@ -14,19 +14,19 @@ const createResourceActions = (namespace: string) => {
     create: data => ({ type: types.CREATE, data }),
     setCreateProgress: () => ({ type: types.CREATE_PROGRESS }),
     setCreateSuccess: () => ({ type: types.CREATE_SUCCESS }),
-    setCreateError: (error) => ({ type: types.CREATE_ERROR, error }),
+    setCreateError: error => ({ type: types.CREATE_ERROR, error }),
     resetCreateStatus: () => ({ type: types.RESET_CREATE_STATUS }),
 
     update: data => ({ type: types.UPDATE, data }),
     setUpdateProgress: () => ({ type: types.UPDATE_PROGRESS }),
     setUpdateSuccess: () => ({ type: types.UPDATE_SUCCESS }),
-    setUpdateError: (error) => ({ type: types.UPDATE_ERROR, error }),
+    setUpdateError: error => ({ type: types.UPDATE_ERROR, error }),
     resetUpdateStatus: () => ({ type: types.RESET_UPDATE_STATUS }),
 
     remove: data => ({ type: types.REMOVE, data }),
     setRemoveProgress: () => ({ type: types.REMOVE_PROGRESS }),
     setRemoveSuccess: () => ({ type: types.REMOVE_SUCCESS }),
-    setRemoveError: (error) => ({ type: types.REMOVE_ERROR, error }),
+    setRemoveError: error => ({ type: types.REMOVE_ERROR, error }),
     resetRemoveStatus: () => ({ type: types.RESET_REMOVE_STATUS }),
   }
 
